79d202e1e84474618de962307dfe5a15dc5dd052
Guneet V <guneet.v@proton.me>
2026-02-28 22:22:14 +0000

    Reordered

---
+++ top50Markets.py
@@ -0,0 +1,28 @@
+from bet import Bet
+import requests
+
+BASE = "https://gamma-api.polymarket.com"
+
+params = {
+    "tag_id": 82,
+    "active": "true",
+    "closed": "false",
+    "order": "volume",
+    "ascending": "false",
+    "limit": 50
+}
+
+events = requests.get(f"{BASE}/events", params=params).json()
+
+bets = []
+
+for event in events:
+    id = event["id"]
+    question = event["title"]
+    volume = event["volume"]
+    startDate = event["startDate"]
+
+    bet = Bet(id, question, volume, startDate)
+
+    bets.append(bet)
+
