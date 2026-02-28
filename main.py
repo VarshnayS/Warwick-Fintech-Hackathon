60328213662546035415cc3a45f9b8767cb728f8
VarshnayS <141043645+VarshnayS@users.noreply.github.com>
2026-02-28 23:29:47 +0000

    matches

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from extractor import extract_bet_info
+from extractor import extract_teams
 from top50Markets import FindTop50Markets
 from speculator import scrape_posts, SUBREDDIT
 
@@ -6,19 +6,20 @@ if __name__ == "__main__":
     bets = FindTop50Markets()
 
     for bet in bets:
-        keyword = extract_bet_info(bet.question)
+        keywords = extract_teams(bet.question)
         start_date = bet.startDate[:10]
 
         total_matches = 0 
         seen = set()    
 
-        for k in keyword: 
+        for k in keywords: 
             posts = scrape_posts(SUBREDDIT, k, start_date)
             for p in posts: 
                 # dont add already seen posts
                 if p["id"] not in seen:
                     seen.add(p["id"])
                     total_matches += 1
+
     print(f"Bet        : {bet.question}")
     print(f"Keywords   : {keywords}")
     print(f"Start date : {start_date}")
